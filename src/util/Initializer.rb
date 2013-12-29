5c43eb15a1117b435a5e26d27caeccbddd5762cf
kabbotta <08kabbotta80@gmail.com>
2013-12-28 19:12:04 -0600

    Create Initializer.rb

---
+++ src/util/Initializer.rb
@@ -0,0 +1,25 @@
+require 'java'
+
+Dir["libs/\*.jar"].each { |jar| require jar }
+
+java_import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+
+java_import com.badlogic.gdx.Gdx
+java_import com.badlogic.gdx.Game
+java_import com.badlogic.gdx.Screen
+java_import com.badlogic.gdx.graphics.GL20
+java_import com.badlogic.gdx.graphics.OrthographicCamera
+java_import com.badlogic.gdx.graphics.g2d.TextureRegion
+java_import com.badlogic.gdx.graphics.g2d.TextureAtlas
+java_import com.badlogic.gdx.assets.AssetManager
+java_import com.badlogic.gdx.assets.loaders.TextureAtlasLoader
+java_import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+java_import com.badlogic.gdx.maps.tiled.TiledMap
+java_import com.badlogic.gdx.maps.tiled.TmxMapLoader
+java_import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+java_import com.badlogic.gdx.math.Vector3
+java_import com.badlogic.gdx.tools.imagepacker.TexturePacker2
+
+require 'src/objects/GameScreen'
+require 'src/util/C'
+require 'src/util/TextureSetup'
